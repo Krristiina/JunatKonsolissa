a7bf64ddfca82aa43fa47caa77dc4b1165c16b02
Your Name <you@example.com>
2017-10-18 12:43:14 +0300

    Lisätään reittihaku ja main-toteutusta

--- src/Kayttoliittyma.java
+++ src/Kayttoliittyma.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 /**
  * Created by Administrator on 17.10.2017.
  */
 public class Kayttoliittyma {
+    public static void main(String[] args) {
+
+        HaeReittijuna();
+    }
+
+    /*Metodi pyytää käyttäjää syöttämään toivotun lähtö ja saapumispaikan. Tämän jälkeen
+    * kutsuu Reittihaku-luokan haeJunatReitille metodia joka tulostaa seuraavat 5 lähtöä*/
+    public static void HaeReittijuna(){
+        String lahto = null;
+        String saapu = null;
+        Scanner sc = new Scanner(System.in);
+
+        try{
+            System.out.println("Syötä lähtöasema: ");
+            lahto = sc.nextLine();
+            System.out.println("Syötä pääteasema: ");
+            saapu = sc.nextLine();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        Reittihaku.haeJunatReitille(lahto, saapu);
+
+    }
+
 }
