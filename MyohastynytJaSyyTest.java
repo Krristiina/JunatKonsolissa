4cbe6cf794ff8fe0f1b98a3b978af06c1bfdd96c
Krristiina <32480997+Krristiina@users.noreply.github.com>
2017-10-25 09:18:29 +0300

    Viimeisin versio

---
+++ MyohastynytJaSyyTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyohastynytJaSyyTest {
+
+    @Test
+    public void testaaLuokanLuonti() throws Exception {
+
+    }
+
+}
